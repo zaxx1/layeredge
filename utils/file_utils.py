9215dbf7c8411fbb5dd472347a246e7b06b9a66b
Mbiphes <castlegima@gmail.com>
2025-02-17 13:08:35 +1000

    fix failed.txt

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,5 +1,5 @@
 import json
-from configs.constants import REGISTER_PATH, FARM_PATH, PROXIES_PATH, FILED_PATH, SUCCESS_PATH, REFS_PATH
+from configs.constants import REGISTER_PATH, FARM_PATH, PROXIES_PATH, FAILED_PATH, SUCCESS_PATH, REFS_PATH
 
 def read_json(path: str, encoding: str | None = None) -> list | dict:
     return json.load(open(path, encoding=encoding))
@@ -21,7 +21,7 @@ def read_refs() -> list[str]:
     return read_file(REFS_PATH)
 
 def write_failed_account(private_key: str):
-    with open(FILED_PATH, 'a', encoding="utf-8") as f:
+    with open(FAILED_PATH, 'a', encoding="utf-8") as f:
         f.write(f'{private_key}\n')
 
 def write_success_account(private_key: str):
